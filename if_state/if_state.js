860c03de17d844a0377f02d801cc0edfda77edb8
sinkerdPhoutthavong <devn.info@gmail.com>
2019-01-29 22:50:57 +0700

    ສໍາເລັດການທົດລອງການໃຊ້ Methods

---
+++ if_state/if_state.js
@@ -0,0 +1,7 @@
+
+var app = new Vue({
+    el:"#if-state",
+    data:{
+        like:false
+    }
+})
---
+++ loop_vue/index.js
@@ -0,0 +1,12 @@
+
+var app = new Vue({
+    el:"#loop",
+    data:{
+        products:["ສະບູ່", "ແປງສີຟັນ", "ຢາສີຟັນ","ຜົງຊັກຟອກ","ອາຫານເສີມ"],
+        employees:[
+            {name:'ທ້າວ ສຸວັນນະສິດ',age:25,address:'Laos'},
+            {name:'ທ້າວ ສິນເກີດ',age:20,address:'Laos'},
+            {name:'ທ້າວ ວົງແສງ',age:35,address:'Laos'}
+        ]
+    }
+})
