1ba18f6591934e7bd73d1429bfdcb799ebc682a7
sinkerdPhoutthavong <devn.info@gmail.com>
2019-02-01 12:44:02 +0700

    ເພີ່ມຄ່າ ຫາຜົນລວມຂອງເງິນທີ່ຕ້ອງຈ່າຍຍ ແລະ ຫາຄ່າສະລ່ຍໂດຍໃຊ່ computed:

---
+++ methods/index.js
@@ -0,0 +1,54 @@
+
+var app = new Vue({
+    el:"#vue-app",
+    data:{
+        newData:{
+            empName:'',
+            salary:0
+        },
+        empGroup:[
+            {
+                empName: "ສິນເກີດ",
+                salary: "25000"
+            },
+            {
+                empName: "ມິຣິນດາ",
+                salary: "28000"
+            }
+        ]
+    },
+    methods:{
+        // ເມັດຕອດສາມາດຮັບຄ່າໄດ້
+        addEmp:function(empName,salary){
+            this.empGroup.push({
+                empName: this.newData.empName,
+                salary: this.newData.salary
+            });
+            // ຫຼັງຈາກປ້ອນແລ້ວ set default
+            this.newData.empName='';
+            this.newData.salary=0;
+        }
+    },
+    computed:{
+            //ແຍກມາຈັດການດ້ານການຄໍານວນ
+            summation: function(){
+                var sum = this.empGroup.reduce(function(value,data){
+                    return value + Number(data.salary);
+                    //0+5000=5000
+                    //5000+4000 = 9000
+                },0);
+                return sum;
+            },
+            Average: function(){
+                var sum = this.empGroup.reduce(function(value,data){
+                    return value + Number(data.salary);
+                    //0+5000=5000
+                    //5000+4000 = 9000
+                },0);
+                console.log(this.empGroup.length); //ສະແດງພະນັກງານພາຍໃນ  empGroup ມີຈັກຄົນ
+                console.log(sum/this.empGroup.length);
+                return sum/this.empGroup.length;
+            }
+        }
+    
+})
---
+++ methods/index.vue
@@ -0,0 +1,31 @@
+
+var app = new Vue({
+    el:"#employees_salary",
+    data:{
+        newData: {
+            empName:'',
+            salary: 0
+        },
+        empGroup:[
+            {
+                empName: "ສິນເກີດ",
+                salary: "25000B"
+            },
+            {
+                empName: "ມິຣິນດາ",
+                salary: "28000B"
+            }
+        ]
+    },
+    methods:{
+        addEmp:function(empName,salary){
+            this.empGroup.push({
+                empName: this.newData.empName,
+                salary: this.newData.salary
+            });
+            // ຫຼັງຈາກປ້ອນແລ້ວ set default
+            this.newData.empName='';
+            this.newData.salary=0;
+        }
+    }
+})
